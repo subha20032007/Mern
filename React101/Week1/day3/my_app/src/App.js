6ba62d15d3f7df4bd36cdb1ac2bd4a03ea436485
unknown <meunknown466@gmail.com>
2023-06-23 23:58:02 +0530

    qe

--- React101/Week1/day3/my_app/src/App.js
+++ React101/Week1/day3/my_app/src/App.js
@@ -40,7 +40,7 @@ function App() {
     console.log(el.status)
   }
   return (
-    //12345678910111213141516171819202122
+    //1
   <div className="App">
      <h1>TODO APP</h1>
      <Addtodo handelAdd={handelAdd}/>
