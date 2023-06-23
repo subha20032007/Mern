aede2f064f010c1e1361211aac9d2311312f486f
unknown <meunknown466@gmail.com>
2023-06-23 23:28:18 +0530

    aD

--- React101/Week1/day3/my_app/src/App.js
+++ React101/Week1/day3/my_app/src/App.js
@@ -1,12 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
-function App() {
+function Addtodo({handelAdd}){
+  const [text,setText]=useState("")
+  const handelChange=(e)=>{
+    setText(e.target.value)
+
+  }
+  const handelClick=()=>{
+  handelAdd(text)
+  setText("")
+  }
   return (
-    <div className="App">
+    <>
+ 
+  <input value={text} type="test" onChange={handelChange}/>
+  <button onClick={handelClick}>ADD</button>
+
+
+    </>
+ 
+  )
+}
+function App() {
+ 
+  const [todo,setTodo]=useState([])
+ 
+  const handelAdd=(text)=>{
+       let newItem={
+        titel:text,
+        status:false
+       }
+       const addItem=[...todo,newItem]
+       setTodo(addItem)
+    
+  }
+  const handelToggle =(el)=>{
+    console.log(el.status)
+  }
+  return (
+    //1
+  <div className="App">
      <h1>TODO APP</h1>
-     <p>1234567891011121314</p>
-    </div>
+     <Addtodo handelAdd={handelAdd}/>
+    
+    {todo.map((el,i)=>(
+
+       <div onClick={handelToggle(el)}key={i}>{el.titel} - {el.status ? "Completed" : "Not Completed"}</div>
+    ))}
+
+</div>
   );
 }
 
