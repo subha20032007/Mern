51c2be041206877035dd61179b35698223defc84
unknown <meunknown466@gmail.com>
2023-06-25 23:55:15 +0530

    Ad

--- React101/Week1/day3/my_app/src/Components/Todo.jsx
+++ React101/Week1/day3/my_app/src/Components/Todo.jsx
@@ -3,7 +3,7 @@ import { AddTodo } from './Components/AddTodo';
 import { useState } from 'react';
 export function Todo(){
     const [todo,setTodo]=useState([])
- //12345678
+ //123456789
   const handelAdd=(text)=>{
        let newItem={
         title:text,
